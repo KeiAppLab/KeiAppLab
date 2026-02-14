79b93201b0d682a95fc12f7430fcb958b457b2b6
KeiAppLab <keiapplab@gmail.com>
2026-02-14 20:50:34 +0900

    Update news_data.js

--- news_data.js
+++ news_data.js
@@ -1,1 +1,11 @@
-
+const newsData = [
+    {
+        date: "2026.02.04",
+        category: "press",
+        title_ja: "発信通話管理APP",
+        title_en: "CallDurationMonitor",
+        url: "#",
+        image: ""
+    },
+  
+];
