0e255587594443c02d22f331f1dcef47c157bf9c
papa-chan <128202940+papa-chan@users.noreply.github.com>
2026-02-17 04:53:11 +0900

    Update news_data.js from Admin Cockpit

--- news_data.js
+++ news_data.js
@@ -1,11 +1,23 @@
+// このファイルは admin.html で生成されました
 const newsData = [
     {
-        date: "2026.02.04",
-        category: "press",
-        title_ja: "発信通話管理APP",
-        title_en: "CallDurationMonitor",
-        url: "#",
-        image: ""
+        "id": 1771271575656,
+        "date": "2026.02.17",
+        "category": "release",
+        "project": "ごちです。",
+        "title": "きた～どうだ～。まじなんです。うそ～。",
+        "body": "そんなこともあるよね。\nだがしかし、なにがや！\nそやかし～、わしも武士の端くれ輩の～",
+        "projectEn": "",
+        "titleEn": "",
+        "bodyEn": "",
+        "image": "index_about.png"
     },
-  
+    {
+        "date": "2026.02.04",
+        "category": "press",
+        "title_ja": "発信通話管理APP",
+        "title_en": "CallDurationMonitor",
+        "url": "#",
+        "image": ""
+    }
 ];
