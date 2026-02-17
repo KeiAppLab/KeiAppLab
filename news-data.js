e0b16f7dd21c5742fd6972fed78689b339d19d34
papa-chan <128202940+papa-chan@users.noreply.github.com>
2026-02-17 17:43:51 +0900

    Update news-data.js (Append)

--- news-data.js
+++ news-data.js
@@ -1,6 +1,18 @@
 // このファイルは admin.html で生成されました
 const newsData = [
     {
+        "id": 1771317819198,
+        "date": "2026.02.17",
+        "category": "update",
+        "project": "起業",
+        "project_en": "",
+        "title": "とうとう始まったやばいぞやばいぞ！起業なんだよ。",
+        "title_en": "",
+        "body": "とうとう始まったんだよ。すげえだろ！\n成功するか分からないけど、夢が詰まってる。\nお互いの弱い部分をお互いが補って、やばいチームって感じ！\n飛び出てるぞミニゲーム",
+        "body_en": "",
+        "image": "index_phot/index_play.png"
+    },
+    {
         "id": 1771263232783,
         "date": "2026.02.17",
         "category": "release",
