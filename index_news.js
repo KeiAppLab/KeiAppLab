1f219f3f8c4f101ea739a0283a40994ae65b0e4d
papa-chan <128202940+papa-chan@users.noreply.github.com>
2026-02-20 10:46:58 +0900

    Update index_news

--- index_news.js
+++ index_news.js
@@ -1,1 +1,19 @@
-const indexNewsData = [];
+const indexNewsData = [
+    {
+        "id": 1771552017013,
+        "date": "2026.02.03",
+        "category": "start",
+        "image": "outgoing_call_widget/index.png",
+        "link": "outgoing_call_widget/index.html",
+        "project_ja": "発信電話ウィジェット",
+        "project_en": "Outgoing Call Widget",
+        "project_de": "Anruf-Monitor Widget",
+        "project_fr": "Widget d'Appels Sortants",
+        "project_es": "",
+        "title_ja": "70分等の無料通話時間を管理する「発信電話ウィジェット」の開発を開始しました。\n",
+        "title_en": "We have launched the development of the \"Outgoing Call Widget,\" designed to manage free calling plans such as 70-minute bundles.",
+        "title_de": "Wir haben mit der Entwicklung des „Anruf-Monitor Widgets“ begonnen, um Freiminuten-Pakete (z. B. 70 Minuten) komfortabel zu verwalten.",
+        "title_fr": "Nous avons lancé le développement du « Widget d'Appels Sortants », conçu pour gérer les forfaits d'appels gratuits comme les offres de 70 minutes.",
+        "title_es": ""
+    }
+];
