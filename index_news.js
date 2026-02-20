48d1341494933f06a7499b6d7d989dc3dd7173ff
papa-chan <128202940+papa-chan@users.noreply.github.com>
2026-02-20 23:23:14 +0900

    Update index_news.js

--- index_news.js
+++ index_news.js
@@ -1,22 +1,5 @@
 const indexNewsData = [
     {
-        "id": 1771582341484,
-        "date": "2026.02.20",
-        "category": "start",
-        "image": "outgoing_call_widget/index.png",
-        "link": "outgoing_call_widget/index.html",
-        "project_ja": "発信電話ウィジェット",
-        "project_en": "Outgoing Call Widget",
-        "project_de": "Anruf-Monitor Widget",
-        "project_fr": "Widget d'Appels Sortants",
-        "project_es": "Widget de Llamadas Salientes",
-        "title_ja": "70分等の無料通話時間を管理する「発信電話ウィジェット」の開発を開始しました。\n",
-        "title_en": "We have launched the development of the \"Outgoing Call Widget,\" designed to manage free calling plans such as 70-minute bundles.",
-        "title_de": "Wir haben mit der Entwicklung des „Anruf-Monitor Widgets“ begonnen, um Freiminuten-Pakete (z. B. 70 Minuten) komfortabel zu verwalten.",
-        "title_fr": "Nous avons lancé le développement du « Widget d'Appels Sortants », conçu pour gérer les forfaits d'appels gratuits comme les offres de 70 minutes.",
-        "title_es": "Hemos iniciado el desarrollo del \"Widget de Llamadas Salientes\", diseñado para gestionar planes de llamadas gratuitas como el de 70 minutos."
-    },
-    {
         "id": 1771552017013,
         "date": "2026.02.03",
         "category": "start",
@@ -26,12 +9,13 @@ const indexNewsData = [
         "project_en": "Outgoing Call Widget",
         "project_de": "Anruf-Monitor Widget",
         "project_fr": "Widget d'Appels Sortants",
-        "project_es": "",
+        "project_es": "Salientes",
+        "title_ja": "70分等の無料通話時間を",
         "title_ja": "70分等の無料通話時間を管理する「発信電話ウィジェット」の開発を開始しました。\n",
         "title_en": "We have launched the development of the \"Outgoing Call Widget,\" designed to manage free calling plans such as 70-minute bundles.",
         "title_de": "Wir haben mit der Entwicklung des „Anruf-Monitor Widgets“ begonnen, um Freiminuten-Pakete (z. B. 70 Minuten) komfortabel zu verwalten.",
         "title_fr": "Nous avons lancé le développement du « Widget d'Appels Sortants », conçu pour gérer les forfaits d'appels gratuits comme les offres de 70 minutes.",
-        "title_es": ""
+        "title_es": "Hemos iniciado el desarrollo del \"Widget de Llamadas Salientes\", diseñado para gestionar planes de llamadas gratuitas como el de 70 minutos."
     },
     {
         "id": 1771595659004,
