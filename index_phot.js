c997cc38b0c28aa20a58ec4c5e535e6de4e8d364
KeiAppLab <keiapplab@gmail.com>
2026-02-20 22:18:56 +0900

    Update index_phot.js

--- index_phot.js
+++ index_phot.js
@@ -3,11 +3,11 @@
 const photoList = [
     { file: "index_phot/app-image.png", name: "アプリイメージ" },
     { file: "index_phot/lab-image.png", name: "ラボ風景" },
-    "Outgoing Call Widget.png",
     "index_about.png",
     "index_port.png",
     "sample_game.jpg",
     "coming_soon.png",
     "index_play.png",
+    "Outgoing_Call_Widget.png",
     // ここに画像を追加していく
 ];
