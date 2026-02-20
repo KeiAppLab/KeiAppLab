70d317805ec454cf7210f1c9c1b10e6c5a737f24
papa-chan <128202940+papa-chan@users.noreply.github.com>
2026-02-21 06:47:58 +0900

    Update kei_applab archive

---
+++ kei_applab/old_news.js
@@ -0,0 +1,11 @@
+const oldNewsData = [
+    {
+        "date": "2026.02.12",
+        "category": "start",
+        "title_ja": " この度、屋号として新たな一歩を踏み出し、起業することを決意いたしました。",
+        "title_en": " I have officially decided to launch my own business under the trade name KEI AppLab.",
+        "title_de": "",
+        "title_fr": "",
+        "title_es": ""
+    }
+];
