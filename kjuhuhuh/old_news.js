825db8dd2fe882a93ba8414ce271e0f677b7281f
papa-chan <128202940+papa-chan@users.noreply.github.com>
2026-02-23 07:25:41 +0900

    Update kjuhuhuh archive

--- kjuhuhuh/old_news.js
+++ kjuhuhuh/old_news.js
@@ -7,5 +7,14 @@ const oldNewsData = [
         "title_de": "",
         "title_fr": "",
         "title_es": ""
+    },
+    {
+        "date": "2026.02.23",
+        "category": "update",
+        "title_ja": "321321321321321",
+        "title_en": "",
+        "title_de": "",
+        "title_fr": "",
+        "title_es": ""
     }
 ];
