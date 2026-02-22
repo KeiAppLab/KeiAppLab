c659c8507b036c9674ab59721cbcd26cecb1942a
papa-chan <128202940+papa-chan@users.noreply.github.com>
2026-02-23 00:43:15 +0900

    Update test_gorira archive

---
+++ test_gorira/old_news.js
@@ -0,0 +1,11 @@
+const oldNewsData = [
+    {
+        "date": "2026.02.23",
+        "category": "update",
+        "title_ja": "どうなってる？大丈夫？",
+        "title_en": "",
+        "title_de": "",
+        "title_fr": "",
+        "title_es": ""
+    }
+];
