dbb35f7b84b0e4edb7e201180243fbc1adc8cfa5
papa-chan <128202940+papa-chan@users.noreply.github.com>
2026-02-26 05:45:38 +0900

    Update qqqqq news

---
+++ qqqqq/news.js
@@ -0,0 +1,11 @@
+const newsData = [
+    {
+        "date": "2026.02.26",
+        "category": "update",
+        "title_ja": "654654654",
+        "title_en": "",
+        "title_de": "",
+        "title_fr": "",
+        "title_es": ""
+    }
+];
